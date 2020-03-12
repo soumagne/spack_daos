bb36208bf7413ef7469ded265a3f4b43a62b6323
Jerome Soumagne <jsoumagne@hdfgroup.org>
2020-03-12 17:31:09 -0500

    Update isa-l package

--- packages/isa-l/package.py
+++ packages/isa-l/package.py
@@ -28,6 +28,7 @@ class IsaL(AutotoolsPackage):
     version('master', branch='master')
 
     # Current
+    version('2.29.0', sha256='832d9747ef3f0c8c05d39e3d7fd6ee5299a844e1ee7382fc8c8b52a268f36eda')
     version('2.28.0', sha256='589202efdcfe437b1786750ec81bf93055e3b88a4bdf909d3b519f2a7134034b')
     version('2.27.0', sha256='d398c5072e8e73bebf02ad4f1db3c13e27a7d96c8c1630e75a19c9bd79a92964')
     version('2.26.0', sha256='938ccce1764ed8fb65a13b02295be5af9a5e0d91686efb7474bde666214153b3')
